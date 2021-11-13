267836976225612ad7e8f6d3c5cd8049037ecd08
HosnainRafi <rhosnain@gmail.com>
2021-11-13 18:28:22 +0600

    Half

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')//cors for own server connected with own
 const app = express();
+const ObjectId = require('mongodb').ObjectId;
 require("dotenv").config();//dotenv config
 const port = process.env.PORT || 5000;
 
@@ -14,33 +15,69 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-    try {
-      await client.connect();
-      const database = client.db("Road-eagles");
-      const usersCollection = database.collection("user");
-      const servicesCollection = database.collection("services");
-      
-      //getServices
-      app.get('/services', async (req, res) => {
-        const cursor = servicesCollection.find({});
-        const services = await cursor.toArray();
-        // console.log(services);
-        res.send(services);
+  try {
+    await client.connect();
+    const database = client.db("Road-eagles");
+    const usersCollection = database.collection("user");
+    const servicesCollection = database.collection("services");
+    const eventsCollection = database.collection("events");
+
+    //getServices
+    app.get('/services', async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      // console.log(services);
+      res.send(services);
     })
-      
-    }
-     finally {
-    //   await client.close();
-    }
+
+    //Save user
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+
+    })
+
+    //Google Sign In update
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const option = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(filter, updateDoc, option);
+      res.json(result);
+    })
+
+    // add events
+    app.post("/addEvent", async (req, res) => {
+      console.log(req.body);
+      const result = await eventsCollection.insertOne(req.body);
+      // console.log(result);
+      res.json(result);
+    });
+
+    //Find single Service
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.send(service);
+  })
+
   }
-  run().catch(console.dir);
+  finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
-app.get('/',(req,res) =>{
-    res.send('Server is ok')
+app.get('/', (req, res) => {
+  res.send('Server is ok')
 });
 
-app.listen(port, () =>{
-    console.log('Port is Ok');
+app.listen(port, () => {
+  console.log('Port is Ok');
 })
